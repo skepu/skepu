7bce1da2e98982ec737520221ca9b01c2aec4779
Johan Ahlqvist <zogoth@gmail.com>
2020-08-05 11:45:25 +0200

    MapPairs, MapPairsReduce skeletons support in StarPU-MPI backend.

--- examples/mappairs.cpp
+++ examples/mappairs.cpp
@@ -10,19 +10,28 @@ int uf1(int a, int b)
 void test1(size_t Vsize, size_t Hsize)
 {
 	auto pairs = skepu::MapPairs(uf1);
-	
+
 	skepu::Vector<int> v1(Vsize, 3), h1(Hsize, 7);
-	
-	for (int i = 0; i < Vsize; ++i) v1(i) = i+1;
-	for (int i = 0; i < Hsize; ++i) h1(i) = (i+1)*10;
-	
-	std::cout << "\nv1: " << v1 << "\nh1: " << h1 << "\n\n";
-	
+
+	skepu::external([&]()
+		{
+			for(int i = 0; i < Vsize; ++i) v1(i) = i+1;
+			for(int i = 0; i < Hsize; ++i) h1(i) = (i+1)*10;
+
+			std::cout << "\nv1: " << v1 << "\nh1: " << h1 << "\n\n";
+		},
+		skepu::write(v1, h1));
+
 	skepu::Matrix<int> res(Vsize, Hsize);
-	
+
 	pairs(res, v1, h1);
-	
-	std::cout << "\ntest 1 res: " << res;
+
+	skepu::external(
+		skepu::read(res),
+		[&]()
+		{
+			std::cout << "\ntest 1 res: " << res;
+		});
 }
 
 
@@ -36,15 +45,20 @@ int uf2(skepu::Index2D i, int ve1, int ve2, int ve3, int he1, int he2, skepu::Ve
 void test2(size_t Vsize, size_t Hsize)
 {
 	auto pairs2 = skepu::MapPairs<3, 2>(uf2);
-	
+
 	skepu::Vector<int> v1(Vsize), v2(Vsize), v3(Vsize);
 	skepu::Vector<int> h1(Hsize), h2(Hsize);
 	skepu::Vector<int> testarg(10);
 	skepu::Matrix<int> res(Vsize, Hsize);
-	
+
 	pairs2(res, v1, v2, v3, h1, h2, testarg, 10, 2);
-	
-	std::cout << "\ntest 2 res: " << res << "\n";
+
+	skepu::external(
+		skepu::read(res),
+		[&]()
+		{
+			std::cout << "\ntest 2 res: " << res << "\n";
+		});
 }
 
 
@@ -57,12 +71,17 @@ int uf3(skepu::Index2D i, int u)
 void test3(size_t Vsize, size_t Hsize)
 {
 	auto pairs3 = skepu::MapPairs<0, 0>(uf3);
-	
+
 	skepu::Matrix<int> res(Vsize, Hsize);
-	
+
 	pairs3(res, 2);
-	
-	std::cout << "\ntest 3 res: " << res << "\n";
+
+	skepu::external(
+		skepu::read(res),
+		[&]()
+		{
+			std::cout << "\ntest 3 res: " << res << "\n";
+		});
 }
 
 
@@ -85,33 +104,33 @@ int uf4(skepu::Index2D i, skepu::Vec<int> test, int u)
 void testReduce(size_t Vsize, size_t Hsize)
 {
 	auto pairs = skepu::MapPairsReduce(uf5, sumd);
-	
+
 	skepu::Vector<int> v1(Vsize, 3), h1(Hsize, 7);
 	skepu::Vector<int> resV(Vsize), resH(Hsize);
-	
+
 	for (int i = 0; i < Vsize; ++i) v1(i) = i+1;
 	for (int i = 0; i < Hsize; ++i) h1(i) = (i+1)*10;
-	
+
 	std::cout << "\nv1: " << v1 << "\nh1: " << h1 << "\n\n";
-	
+
 	pairs.setReduceMode(skepu::ReduceMode::ColWise);
 	pairs(resH, v1, h1);
 	std::cout << "\nresH: " << resH << "\n";
-	
+
 	pairs.setReduceMode(skepu::ReduceMode::RowWise);
 	pairs(resV, v1, h1);
 	std::cout << "\nresV: " << resV << "\n";
-	
-	
+
+
 	// Test implicit dimensions
 	{
 		auto pairs4 = skepu::MapPairsReduce<0, 0>(uf4, sumd);
-		
+
 		pairs4.setDefaultSize(Vsize, Hsize);
 		pairs4.setReduceMode(skepu::ReduceMode::ColWise);
 		pairs4(resH, v1, 0);
 		std::cout << "\nresH: " << resH << "\n";
-		
+
 		pairs4.setReduceMode(skepu::ReduceMode::RowWise);
 		pairs4(resV, v1, 0);
 		std::cout << "\nresV: " << resV << "\n";
@@ -128,19 +147,17 @@ int main(int argc, char *argv[])
 		std::cout << "Usage: " << argv[0] << " size_v size_h backend\n";
 		exit(1);
 	}
-	
+
 	const size_t Vsize = atoi(argv[1]);
 	const size_t Hsize = atoi(argv[2]);
 	auto spec = skepu::BackendSpec{skepu::Backend::typeFromString(argv[3])};
 	skepu::setGlobalBackendSpec(spec);
-	
+
 	test1(Vsize, Hsize);
-	for (int i = 0; i < 10; ++i) std::cout << std::endl;
 	test2(Vsize, Hsize);
-	
 	test3(Vsize, Hsize);
-	
+
 //	testReduce(Vsize, Hsize);
-	
+
 	return 0;
 }
--- examples/nbody_mappairs.cpp
+++ examples/nbody_mappairs.cpp
@@ -25,16 +25,16 @@ struct Acceleration
 Acceleration influence(skepu::Index2D index, Particle pi, Particle pj)
 {
 	Acceleration acc;
-	
+
 	if (index.row != index.col)
 	{
 		float rij = sqrt((pi.x - pj.x) * (pi.x - pj.x) + (pi.y - pj.y) * (pi.y - pj.y) + (pi.z - pj.z) * (pi.z - pj.z));
 		float dum = G * pi.m * pj.m / pow(rij, 3);
-		
+
 		acc.x = dum * (pi.x - pj.x);
 		acc.y = dum * (pi.y - pj.y);
 		acc.z = dum * (pi.z - pj.z);
-		
+
 	//	std::cout << "rij: " << rij << ", dum: " << dum;
 	//	std::cout << ", (" << index.row << ", " << index.col << "): ax = " << acc.x << ", ay = " << acc.y << ", az = " << acc.z << "\n";
 	}
@@ -59,15 +59,15 @@ Acceleration sum(Acceleration lhs, Acceleration rhs)
 Particle update(Particle p, Acceleration a)
 {
 	Particle res = p;
-	
+
 	res.x += delta_t * p.vx + delta_t * delta_t / 2 * a.x;
 	res.y += delta_t * p.vy + delta_t * delta_t / 2 * a.y;
 	res.z += delta_t * p.vz + delta_t * delta_t / 2 * a.z;
-	
+
 	res.vx += delta_t * a.x;
 	res.vy += delta_t * a.y;
 	res.vz += delta_t * a.z;
-	
+
 	return res;
 }
 
@@ -80,19 +80,19 @@ Particle init(skepu::Index1D index, size_t np)
 	int i = s % np;
 	int j = ((s - i) / np) % np;
 	int k = (((s - i) / np) - j) / np;
-	
+
 	Particle p;
-	
+
 	p.x = i - d + 1;
 	p.y = j - d + 1;
 	p.z = k - d + 1;
-	
+
 	p.vx = 0.0;
 	p.vy = 0.0;
 	p.vz = 0.0;
-	
+
 	p.m = 1;
-	
+
 	return p;
 }
 
@@ -128,7 +128,7 @@ void save_step(skepu::Vector<Particle> &particles, std::ostream &os = std::cout)
 void save_step(skepu::Vector<Particle> &particles, const std::string &filename)
 {
 	std::ofstream out(filename);
-	
+
 	if (out.is_open())
 		save_step(particles, out);
 	else
@@ -144,12 +144,12 @@ void nbody(skepu::Vector<Particle> &particles, size_t iterations, skepu::Backend
 {
 	size_t np = particles.size();
 	skepu::Vector<Acceleration> accel(np);
-	
+
 	if (spec) skepu::setGlobalBackendSpec(*spec);
-	
+
 	// particle vectors initialization
 	nbody_init(particles, np);
-	
+
 	for (size_t i = 0; i < iterations; ++i)
 	{
 		nbody_influence(accel, particles, particles);
@@ -161,22 +161,30 @@ int main(int argc, char *argv[])
 {
 	if (argc < 4)
 	{
-		std::cout << "Usage: " << argv[0] << " particles-per-dim iterations backend\n";
+		skepu::external(
+		[&]{
+			std::cout << "Usage: " << argv[0]
+				<< " particles-per-dim iterations backend\n";
+		});
 		exit(1);
 	}
-	
+
 	const size_t np = std::stoul(argv[1]);
 	const size_t iterations = std::stoul(argv[2]);
 	auto spec = skepu::BackendSpec{skepu::Backend::typeFromString(argv[3])};
-	
+
 	// Particle vectors....
 	skepu::Vector<Particle> particles(np);
-	
+
 	nbody(particles, iterations, &spec);
-	
-	std::stringstream outfile2;
-	outfile2 << "output" << spec.type() << ".txt";
-	save_step(particles, outfile2.str());
-	
+
+	skepu::external(
+		skepu::read(particles),
+		[&]{
+			std::stringstream outfile2;
+			outfile2 << "output" << spec.type() << ".txt";
+			save_step(particles, outfile2.str());
+		});
+
 	return 0;
 }
