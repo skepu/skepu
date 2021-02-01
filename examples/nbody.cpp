d07133949bb01008e41c8995df8662ecd03aeca4
Johan Ahlqvist <johan.ahlqvist@liu.se>
2021-02-01 15:20:47 +0100

    Changed nbody example to use skepu::external.

--- examples/nbody.cpp
+++ examples/nbody.cpp
@@ -27,41 +27,41 @@ constexpr float delta_t [[skepu::userconstant]] = 0.1;
 Particle move(skepu::Index1D index, Particle pi, const skepu::Vec<Particle> parr)
 {
 	size_t i = index.i;
-	
+
 	float ax = 0.0, ay = 0.0, az = 0.0;
 	size_t np = parr.size;
-	
+
 	for (size_t j = 0; j < np; ++j)
 	{
 		if (i != j)
 		{
 			Particle pj = parr[j];
-			
+
 			float rij = sqrt((pi.x - pj.x) * (pi.x - pj.x)
 			               + (pi.y - pj.y) * (pi.y - pj.y)
 			               + (pi.z - pj.z) * (pi.z - pj.z));
-			
+
 			float dum = G * pi.m * pj.m / pow(rij, 3);
-			
+
 			ax += dum * (pi.x - pj.x);
 			ay += dum * (pi.y - pj.y);
 			az += dum * (pi.z - pj.z);
 		}
 	}
-	
+
 //	std::cout << "i = " << i << ": ax = " << ax << ", ay = " << ay << ", az = " << az << "\n";
-	
+
 	Particle newp;
 	newp.m = pi.m;
-	
+
 	newp.x = pi.x + delta_t * pi.vx + delta_t * delta_t / 2 * ax;
 	newp.y = pi.y + delta_t * pi.vy + delta_t * delta_t / 2 * ay;
 	newp.z = pi.z + delta_t * pi.vz + delta_t * delta_t / 2 * az;
-	
+
 	newp.vx = pi.vx + delta_t * ax;
 	newp.vy = pi.vy + delta_t * ay;
 	newp.vz = pi.vz + delta_t * az;
-	
+
 	return newp;
 }
 
@@ -74,19 +74,19 @@ Particle init(skepu::Index1D index, size_t np)
 	int i = s % np;
 	int j = ((s - i) / np) % np;
 	int k = (((s - i) / np) - j) / np;
-	
+
 	Particle p;
-	
+
 	p.x = i - d + 1;
 	p.y = j - d + 1;
 	p.z = k - d + 1;
-	
+
 	p.vx = 0.0;
 	p.vy = 0.0;
 	p.vz = 0.0;
-	
+
 	p.m = 1;
-	
+
 	return p;
 }
 
@@ -122,7 +122,7 @@ void save_step(skepu::Vector<Particle> &particles, std::ostream &os = std::cout)
 void save_step(skepu::Vector<Particle> &particles, const std::string &filename)
 {
 	std::ofstream out(filename);
-	
+
 	if (out.is_open())
 		save_step(particles, out);
 	else
@@ -137,12 +137,12 @@ void nbody(skepu::Vector<Particle> &particles, size_t iterations, skepu::Backend
 {
 	size_t np = particles.size();
 	skepu::Vector<Particle> doublebuffer(particles.size());
-	
+
 	if (spec) skepu::setGlobalBackendSpec(*spec);
-	
+
 	// particle vectors initialization
 	nbody_init(particles, np);
-	
+
 	for (size_t i = 0; i < iterations; i += 2)
 	{
 		nbody_simulate_step(doublebuffer, particles, particles);
@@ -154,26 +154,29 @@ int main(int argc, char *argv[])
 {
 	if (argc < 4)
 	{
-		if(!skepu::cluster::mpi_rank())
-			std::cout << "Usage: " << argv[0] << " particles-per-dim iterations backend\n";
+		skepu::external([&]{
+			std::cout << "Usage: " << argv[0]
+				<< " particles-per-dim iterations backend\n";});
 		exit(1);
 	}
-	
+
 	const size_t np = std::stoul(argv[1]);
 	const size_t iterations = std::stoul(argv[2]);
 	auto spec = skepu::BackendSpec{skepu::Backend::typeFromString(argv[3])};
-	
+
 	// Particle vectors....
 	skepu::Vector<Particle> particles(np);
-	
-	nbody(particles, iterations, &spec);
-	
-	std::stringstream outfile2;
-	outfile2 << "output" << spec.type() << ".txt";
 
-	particles.flush();
-	if(!skepu::cluster::mpi_rank())
-		save_step(particles, outfile2.str());
-	
+	nbody(particles, iterations, &spec);
+
+	skepu::external(
+		skepu::read(particles),
+		[&]{
+			std::stringstream outfile2;
+			outfile2 << "output" << spec.type() << ".txt";
+
+			particles.flush();
+			save_step(particles, outfile2.str());
+		});
 	return 0;
 }
