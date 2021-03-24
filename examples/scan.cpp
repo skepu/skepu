ff9c573b27e382ed4521fed3d999c7f6f74aba46
Johan Ahlqvist <johan.ahlqvist@liu.se>
2021-03-24 13:36:47 +0100

    Scan added to StarPU-MPI.

--- examples/scan.cpp
+++ examples/scan.cpp
@@ -6,55 +6,70 @@ float plus_f(float a, float b)
 	return a + b;
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char ** argv)
 {
-	if (argc < 3)
+	if(argc < 3)
 	{
-		std::cout << "Usage: " << argv[0] << " size backend\n";
-		exit(1);
+		skepu::external([&]{
+			std::cout << "Usage: " << argv[0] << " size backend\n";});
+		std::abort();
 	}
-	
+
 	const size_t size = atoi(argv[1]);
 	auto spec = skepu::BackendSpec{skepu::Backend::typeFromString(argv[2])};
-	
+
 	skepu::Vector<float> v(size), r(size);
 	v.randomize(0, 10);
-	
-	std::cout << "v: " << v << "\n";
-	
+
+	skepu::external(
+		skepu::read(v),
+		[&]{ std::cout << "v: " << v << "\n"; });
+
 	auto prefix_sum = skepu::Scan(plus_f);
 	prefix_sum.setBackend(spec);
-	
+
 	prefix_sum.setStartValue(10);
 	prefix_sum.setScanMode(skepu::ScanMode::Inclusive);
-	
+
 	// With containers
 	prefix_sum(r, v);
-	std::cout << "Scan inclusive: r = " << r << "\n";
-	
+	skepu::external(
+		skepu::read(r),
+		[&]{ std::cout << "Scan inclusive: r = " << r << "\n"; });
+
 	// With iterators
 	prefix_sum(r.begin(), r.end(), v.begin());
-	std::cout << "Scan inclusive: r = " << r << "\n";
-	
+	skepu::external(
+		skepu::read(r),
+		[&]{ std::cout << "Scan inclusive: r = " << r << "\n"; });
+
 	// With mixed iterators
 	prefix_sum(r.begin(), r.end(), v);
-	std::cout << "Scan inclusive: r = " << r << "\n";
-	
-	
+	skepu::external(
+		skepu::read(r),
+		[&]{ std::cout << "Scan inclusive: r = " << r << "\n"; });
+
+
 	prefix_sum.setScanMode(skepu::ScanMode::Exclusive);
-	
+
 	// With containers
 	prefix_sum(r, v);
-	std::cout << "Scan exclusive: r = " << r << "\n";
-	
+	skepu::external(
+		skepu::read(r),
+		[&]{ std::cout << "Scan exclusive: r = " << r << "\n"; });
+
 	// With iterators
 	prefix_sum(r.begin(), r.end(), v.begin());
-	std::cout << "Scan exclusive: r = " << r << "\n";
-	
+	skepu::external(
+		skepu::read(r),
+		[&]{ std::cout << "Scan exclusive: r = " << r << "\n";});
+
 	// With mixed iterators
 	prefix_sum(r, v.begin());
-	std::cout << "Scan exclusive: r = " << r << "\n";
-	
+	skepu::external(
+		skepu::read(r),
+		[&]{ std::cout << "Scan exclusive: r = " << r << "\n"; });
+
 	return 0;
 }
 
